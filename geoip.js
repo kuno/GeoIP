85b1cec78cfe61f30a7628f8b73b9e095ccbf22a
kuno <neokuno@gmail.com>
2010-11-23 12:05:53 +0800

    prepare re-factory

--- geoip.js
+++ geoip.js
@@ -1,3 +1,3 @@
-exports.GeoIP = require('./lib/geobj.js');
+exports.GEOIP = require('./lib/geobj.js').GEOIP;
 exports.country = require('./lib/country.js');
 exports.city = require('./lib/city.js');
