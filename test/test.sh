d57533b1845ccfe24c6c033de198fb24771ce980
kuno <neokuno@gmail.com>
2010-12-03 14:22:22 +0800

    added automatic testing scripts

--- test/test.sh
+++ test/test.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env sh
 
-GEOIP = 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz'
-GEOLITECITY = 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz'
+GEOIP='http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz'
+GEOLITECITY='http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz'
 
 
-if [ ! -e /tmp/GeoIP.dat ]; then
-    wget $GEOIP /tmp/GeoIP.dat.gz || gzip -d /tmp/GeoIP.dat.gz
-elif [ ! -e /tmp/GeoLiteCity.dat ]; then
-    wget $GEOLITECITY ../data/GeoLiteCity.dat.gz || gzip -d /tmp/GeoLiteCity.dat.gz
+if [ ! -e /tmp/GeoIP.dat ] || [! -e /tmp/GeoLiteCity.dat ]; then
+    curl $GEOIP | gzip -d -c > /tmp/GeoIP.dat
+    curl $GEOLITECITY | gzip -d -c > /tmp/GeoLiteCity.dat
 fi
 
-node ./country.js
-#node ./city.js    || return 1
-#node ./org.js     || return 1
+echo "Start to test Country module......"
+echo "============================================================"
+node ./country.js 
+echo "============================================================"
+echo "Country module test is finished!"
+
+echo ""
+
+echo "Start to test City moduel......"
+echo "============================================================"
+node ./city.js
+echo "============================================================"
+echo "City moduel test is finished!"
 
 
