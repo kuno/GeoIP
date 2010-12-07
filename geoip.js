ed239688b45d05cce197fce7193259816d9a0078
kuno <neokuno@gmail.com>
2010-12-07 09:46:33 +0800

    fix constants name bug in geoip.js

--- geoip.js
+++ geoip.js
@@ -9,53 +9,55 @@ DATA = require('./include/models.js').DATA;
  *****************************************************************************/
 
  function _setup_segments(data) {
-     var offset;
-     data.db_type = CONST.COUNTRY_EDITION;
-     data.record_length = CONST.STANDARD_RECORD_LENGTH;
-     var buf = data.buffer;
-     offset = buf.length - 3;
-     for (var i = 0; i < CONST.STRUCTURE_INFO_MAX_SIZE; i++) {
-         var delim = buf2long(buf.slice(offset, offset + 3));
-         offset += 3;
-         if (delim === CONST.DELIMETER_NUMBER) {
-             data.db_type = parseInt(buf[offset], 10);
-             offset++;
-             if (data.db_type === CONST.REGION_EDITION_REV0) {
-                 data.db_segments = CONST.STATE_BEGIN_REV0;
-             } else if (data.db_type === CONST.REGION_EDITION_REV1) {
-                 data.db_segments = CONST.STATE_BEGIN_REV1;
-             } else if (data.db_type === CONST.CITY_EDITION_REV0 ||
-             data.db_type === CONST.CITY_EDITION_REV1 ||
-             data.db_type === CONST.CITY_ORG_EDITION  ||
-             data.db_type === CONST.CITY_DOMAIN_EDITION ||
-             data.db_type === CONST.CITY_ISP_EDITION    ||
-             data.db_type === CONST.LOCATIONA_EDITION   ||
-             data.db_type === CONST.ACCURACYRADIUS_EDITION ||
-             data.db_type === CONST.GEIOP_ASNUM_EDITION) {
-                 data.db_segments = 0;
-                 var seg = buf.slice(offset, offset + CONST.SEGMENT_RECORD_LENGTH);
-                 for (var j = 0; j < CONST.SEGMENT_RECORD_LENGTH; j++) {
-                     data.db_segments += (parseInt(seg[j], 10) << (j * 8));
-                 }
-                 if (data.db_type === CONST.ORG_EDITION ||
-                 data.db_type === CONST.DOMAIN_EDITION ||
-                 data.db_type === CONST.ISP_EDITION) {
-                     data.record_length = CONST.ORG_RECORD_LENGTH;
-                 }
-             }
-             break;
-         } else {
-             offset -= 4;
-         }
+   var offset;
+   data.db_type = CONST.COUNTRY_EDITION;
+   data.record_length = CONST.STANDARD_RECORD_LENGTH;
+   var buf = data.buffer;
+   offset = buf.length - 3;
+   for (var i = 0; i < CONST.STRUCTURE_INFO_MAX_SIZE; i++) {
+     var delim = buf2long(buf.slice(offset, offset + 3));
+     offset += 3;
+     if (delim === CONST.DELIMETER_NUMBER) {
+       data.db_type = parseInt(buf[offset], 10);
+       offset++;
+       if (data.db_type === CONST.REGION_EDITION_REV0) {
+         data.db_segments = CONST.STATE_BEGIN_REV0;
+       } else if (data.db_type === CONST.REGION_EDITION_REV1) {
+         data.db_segments = CONST.STATE_BEGIN_REV1;
+       } else if (data.db_type === CONST.CITY_EDITION_REV0 ||
+       data.db_type === CONST.CITY_EDITION_REV1 ||
+       data.db_type === CONST.ORG_EDITION  ||
+       data.db_type === CONST.DOMAIN_EDITION ||
+       data.db_type === CONST.ISP_EDITION    ||
+       data.db_type === CONST.LOCATIONA_EDITION   ||
+       data.db_type === CONST.ACCURACYRADIUS_EDITION ||
+       data.db_type === CONST.ASNUM_EDITION
+     ) {
+       data.db_segments = 0;
+       var seg = buf.slice(offset, offset + CONST.SEGMENT_RECORD_LENGTH);
+       for (var j = 0; j < CONST.SEGMENT_RECORD_LENGTH; j++) {
+         data.db_segments += (parseInt(seg[j], 10) << (j * 8));
+       }
+       if (data.db_type === CONST.ORG_EDITION ||
+       data.db_type === CONST.DOMAIN_EDITION ||
+       data.db_type === CONST.ISP_EDITION
+     ) {
+       data.record_length = CONST.ORG_RECORD_LENGTH;
      }
+   }
+   break;
+ } else {
+   offset -= 4;
+ }
+   }
 
-     if (data.db_type === CONST.COUNTRY_EDITION ||
-     data.db_type === CONST.PROXY_EDITION   ||
-     data.db_type === CONST.NETSPEED_EDITION) {
-         data.db_segments = CONST.COUNTRY_BEGIN;
-     }
+   if (data.db_type === CONST.COUNTRY_EDITION ||
+   data.db_type === CONST.PROXY_EDITION   ||
+   data.db_type === CONST.NETSPEED_EDITION) {
+     data.db_segments = CONST.COUNTRY_BEGIN;
+   }
 
-     return data;
+   return data;
  }
 
 
@@ -63,23 +65,23 @@ DATA = require('./include/models.js').DATA;
   * Exprots Functions
   *****************************************************************************/
   exports.open = function(file) {
-      var stats, bytesRead;
-      var data = new DATA();
-      data.file_descriptor = fs.openSync(file, 'r');
-      stats = fs.fstatSync(data.file_descriptor);
-      data.buffer = new Buffer(stats.size);
-      bytesRead = fs.readSync(data.file_descriptor, data.buffer, 0, stats.size, 0);
+    var stats, bytesRead;
+    var data = new DATA();
+    data.file_descriptor = fs.openSync(file, 'r');
+    stats = fs.fstatSync(data.file_descriptor);
+    data.buffer = new Buffer(stats.size);
+    bytesRead = fs.readSync(data.file_descriptor, data.buffer, 0, stats.size, 0);
 
-      if (bytesRead >= 0) {
-         return _setup_segments(data);
-      } else {
-          return false;
-      }
+    if (bytesRead >= 0) {
+      return _setup_segments(data);
+    } else {
+      return false;
+    }
 
   };
 
   exports.close = function(data) {
-      return fs.close(data.file_descriptor);
+    return fs.close(data.file_descriptor);
   };
 
 
