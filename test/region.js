8ead20a921445251ba570f4ca62e5090695d966f
kuno <neokuno@gmail.com>
2010-12-07 10:07:12 +0800

    added Region moduel testing script

---
+++ test/region.js
@@ -0,0 +1,12 @@
+var geoip = require('../geoip.js');
+
+var data = geoip.open('/tmp/GeoIPRegion-515_20050401/GeoIPRegion-515.dat');
+
+var Region = geoip.Region;
+
+setTimeout(function() {
+  console.log('The result of ');
+  console.log('Region.region_by_addr(data, \'8.8.8.8\')');
+  console.log('is \'' + Region.region_by_addr(data, '8.8.8.8') + '\'');
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+}, 1000);
