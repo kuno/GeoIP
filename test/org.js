5f8ce650e9393f12198233f3991e377697d14d48
kuno <neokuno@gmail.com>
2010-12-10 09:49:44 +0800

    make org_by_addr return a array of string

--- test/org.js
+++ test/org.js
@@ -5,8 +5,10 @@ var org_data = geoip.open('/tmp/GeoIP-111_20030603/GeoIPOrg-111.dat');
 var Org = geoip.Org;
 
 setTimeout(function() {
+  var result = Org.org_by_addr(org_data, '8.8.8.8');
   console.log('The result of ');
   console.log('Org.org_by_addr(org_data, \'8.8.8.8\')');
-  console.log('is \'' + Org.org_by_addr(org_data, '8.8.8.8.8') + '\'');
+  console.log('is ' + result);
+  console.log('The type of result is ' + (typeof result));
   console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
 }, 1000);
