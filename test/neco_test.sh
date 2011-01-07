0f832a2ee9f86d2ee7e1ddecf483ec3c43cee0c9
kuno <neokuno@gmail.com>
2011-01-07 12:14:57 +0800

    usr for loop in neco_test.sh script

--- test/neco_test.sh
+++ test/neco_test.sh
@@ -1,3 +1,8 @@
+#!/usrbin/env sh
+
+# Note: In order to use this testing script,
+# You need install neco first,
+# See detials at https://github.com/kuno/neco
 source $HOME/utopia/.neco/activate.sh
 
 report=./report.txt
@@ -6,16 +11,43 @@ if [ -e $report ]; then
   rm -rf $report
 fi
 
-neco_activate test0 > /dev/null
-echo '>>>>>>>>>>>>>>>>>>>>Testing on '$ecosystem'<<<<<<<<<<<<<<<<<<<' >> $report
-node -v >> $report
-sh test.sh >> $report
-echo '>>>>>>>>>>>>>>>>>>>>Finished Testing on '$ecosystem '<<<<<<<<<<<<<<<<<<<<<<' >> $report
-neco_deactivate
+# Tesing no nodejs 0.2.x stable branch
+for neco_id in 'test0' 'test1' 'test2' 'test3' 'test4' 'test5' 'test6'; do
+  neco_activate $neco_id &>/dev/null
+  version=$(node -v)
+  echo '#############################################################' >> $report
+  echo '################# Testing on nodejs '$version' ##################' >> $report
+  echo '#############################################################' >> $report
+  sh test.sh >> $report
+  echo '#############################################################' >> $report
+  echo '############## Finished tests on nodejs '$version' ##############' >> $report
+  echo '#############################################################' >> $report
+  echo '' >> $report
+  neco_deactivate
+done
 
-neco_activate test1 > /dev/null
-echo '>>>>>>>>>>>>>>>>>>>>Testing on '$ecosystem'<<<<<<<<<<<<<<<<<<<' >> $report
-node -v >> $report
+# Tesing no nodejs 0.3.x unstalbe branch
+for neco_id in 'test10' 'test11' 'test12'; do
+  neco_activate $neco_id &>/dev/null
+  version=$(node -v)
+  echo '#############################################################' >> $report
+  echo '################## Testing on nodejs '$version' #################' >> $report
+  echo '#############################################################' >> $report
+  sh test.sh >> $report
+  echo '#############################################################' >> $report
+  echo '############## Finished tests on nodejs '$version' ##############' >> $report
+  echo '#############################################################' >> $report
+  echo '' >> $report
+  neco_deactivate
+done
+
+# Tesing on latest version of nodejs
+version=$(node -v)
+echo '#############################################################' >> $report
+echo '################# Testing on nodejs '$version' ##################' >> $report
+echo '#############################################################' >> $report
 sh test.sh >> $report
-echo '>>>>>>>>>>>>>>>>>>>>Finished Testing on '$ecosystem '<<<<<<<<<<<<<<<<<<<<<<' >> $report
-neco_deactivate          
+echo '#############################################################' >> $report
+echo '############## Finished tests on nodejs '$version' ##############' >> $report
+echo '#############################################################' >> $report
+echo '' >> $report  
