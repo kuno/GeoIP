8d8ece6600c30b8ccab2478b2b3c98bbd34d1325
kuno <neokuno@gmail.com>
2011-05-07 14:09:38 +0800

    city class in js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var read  = require('fs').readFileSync,
     join  = require('path').join;
 
-var binding = require('./geoip.node');
+var binding = require('./geoip.node'),
+    City    = require('./lib/city.js');
 
 exports.Country = binding.Country;
-exports.City  = binding.City;
+exports.City    = City;
 
 exports.version = JSON.parse(read(join(__dirname, 'package.json'))).version;
