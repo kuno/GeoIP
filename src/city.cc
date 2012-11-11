044fefbbd9989ee59b0f08368ebe8ba022345e7f
Sajal Kayan <sajal@thaindian.com>
2012-11-12 02:17:11 +0700

    reduce leakage in geoip::City, still leaky

--- src/city.cc
+++ src/city.cc
@@ -83,6 +83,7 @@ Handle<Value> geoip::City::lookupSync(const Arguments &args) {
   GeoIPRecord * record = GeoIP_record_by_ipnum(c->db, ipnum);
 
   if (record == NULL) {
+    delete record;
     return scope.Close(Null()); //return ThrowException(String::New("Error: Can not find match data"));
   }
 
@@ -141,6 +142,7 @@ Handle<Value> geoip::City::lookupSync(const Arguments &args) {
     }
   }
 
+  delete record;
   return scope.Close(data);
 }
 
