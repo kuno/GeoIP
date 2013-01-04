704af97cf73003b80c359b89b01e6ad9e3e23160
kuno <neokuno@gmail.com>
2013-01-04 22:58:15 +0800

    add sagfault testing srcript

---
+++ test/sagfault.js
@@ -0,0 +1,10 @@
+var path = require('path');
+var file = path.resolve(__dirname, '../database/GeoLiteCity.dat');
+var geoip = path.resolve(__dirname, '..');
+var city = new (require(geoip).City)(file);
+
+for(var j = 100; --j;){
+  var ip = [1,1,1,1].map(function(){return 0|Math.random()*256}).join('.');
+  console.log(ip);
+  city.lookupSync(ip);
+}
