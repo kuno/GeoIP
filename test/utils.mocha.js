78e0b11910b5ab2e5e069af21c7d53a47c4d7407
Guan Qing <kuno.the.developer@gmail.com>
2013-12-29 12:46:17 +0800

    clean up

--- test/utils.mocha.js
+++ test/utils.mocha.js
@@ -17,103 +17,103 @@ var should = chai.should();
 var geoip = require('../index.js');
 
 describe('Check database file type', function() {
-  describe(path.resolve(__dirname, '../database/GeoIP-171_20040418.dat'), function() {
-    it('should equal to netspeed', function(done) {
-      var file = path.resolve(__dirname, '../database/GeoIP-171_20040418.dat');
-      var type = geoip.check(file);
+    describe(path.resolve(__dirname, '../database/GeoIP-171_20040418.dat'), function() {
+        it('is netspeed db', function(done) {
+            var file = path.resolve(__dirname, '../database/GeoIP-171_20040418.dat');
+            var type = geoip.check(file);
 
-      type.should.equal('netspeed');
-      setTimeout(done, 1);
+            type.should.equal('netspeed');
+            setTimeout(done, 1);
+        });
     });
-  });
 
-  describe(path.resolve(__dirname, '../database/GeoIPASNum.dat'), function() {
-    it('should equal to asnum', function(done) {
-      var file = path.resolve(__dirname, '../database/GeoIPASNum.dat');
-      var type = geoip.check(file);
+    describe(path.resolve(__dirname, '../database/GeoIPASNum.dat'), function() {
+        it('is asnum db', function(done) {
+            var file = path.resolve(__dirname, '../database/GeoIPASNum.dat');
+            var type = geoip.check(file);
 
-      type.should.equal('asnum');
-      setTimeout(done, 1);
+            type.should.equal('asnum');
+            setTimeout(done, 1);
+        });
     });
-  });
 
-  describe(path.resolve(__dirname, '../database/GeoIP.dat'), function() {
-    it('should equal to country', function(done) {
-      var file = path.resolve(__dirname, '../database/GeoIP.dat');
-      var type = geoip.check(file);
+    describe(path.resolve(__dirname, '../database/GeoIP.dat'), function() {
+        it('is country db', function(done) {
+            var file = path.resolve(__dirname, '../database/GeoIP.dat');
+            var type = geoip.check(file);
 
-      type.should.equal('country');
-      setTimeout(done, 1);
+            type.should.equal('country');
+            setTimeout(done, 1);
+        });
     });
-  });
 
-  describe(path.resolve(__dirname, '../database/GeoIPv6.dat'), function() {
-    it('should equal to country_v6', function(done) {
-      var file = path.resolve(__dirname, '../database/GeoIPv6.dat');
-      var type = geoip.check(file);
+    describe(path.resolve(__dirname, '../database/GeoIPv6.dat'), function() {
+        it('is country_v6 db', function(done) {
+            var file = path.resolve(__dirname, '../database/GeoIPv6.dat');
+            var type = geoip.check(file);
 
-      type.should.equal('country_v6');
-      setTimeout(done, 1);
+            type.should.equal('country_v6');
+            setTimeout(done, 1);
+        });
     });
-  });
 
-  describe(path.resolve(__dirname, '../database/GeoLiteCity.dat'), function() {
-    it('should equal to city', function(done) {
-      var file = path.resolve(__dirname, '../database/GeoLiteCity.dat');
-      var type = geoip.check(file);
+    describe(path.resolve(__dirname, '../database/GeoLiteCity.dat'), function() {
+        it('is city db', function(done) {
+            var file = path.resolve(__dirname, '../database/GeoLiteCity.dat');
+            var type = geoip.check(file);
 
-      type.should.equal('city');
-      setTimeout(done, 1);
+            type.should.equal('city');
+            setTimeout(done, 1);
+        });
     });
-  });
 
-  describe(path.resolve(__dirname, '../database/GeoLiteCityv6.dat'), function() {
-    it('is city_v6 db', function(done) {
-      var file = path.resolve(__dirname, '../database/GeoLiteCityv6.dat');
-      var type = geoip.check(file);
+    describe(path.resolve(__dirname, '../database/GeoLiteCityv6.dat'), function() {
+        it('is city_v6 db', function(done) {
+            var file = path.resolve(__dirname, '../database/GeoLiteCityv6.dat');
+            var type = geoip.check(file);
 
-      type.should.equal('city_v6');
-      setTimeout(done, 1);
+            type.should.equal('city_v6');
+            setTimeout(done, 1);
+        });
     });
-  });
 
-  describe(path.resolve(__dirname, '../database/GeoIPRegion-515.dat'), function() {
-    it('is region db', function(done) {
-      var file = path.resolve(__dirname, '../database/GeoIPRegion-515.dat');
-      var type = geoip.check(file);
+    describe(path.resolve(__dirname, '../database/GeoIPRegion-515.dat'), function() {
+        it('is region db', function(done) {
+            var file = path.resolve(__dirname, '../database/GeoIPRegion-515.dat');
+            var type = geoip.check(file);
 
-      type.should.equal('region');
-      setTimeout(done, 1);
+            type.should.equal('region');
+            setTimeout(done, 1);
+        });
     });
-  });
 
-  describe(path.resolve(__dirname, '../database/GeoIPOrg-111.dat'), function() {
-    it('is org db', function(done) {
-      var file = path.resolve(__dirname, '../database/GeoIPOrg-111.dat');
-      var type = geoip.check(file);
+    describe(path.resolve(__dirname, '../database/GeoIPOrg-111.dat'), function() {
+        it('is org db', function(done) {
+            var file = path.resolve(__dirname, '../database/GeoIPOrg-111.dat');
+            var type = geoip.check(file);
 
-      type.should.equal('org');
-      setTimeout(done, 1);
+            type.should.equal('org');
+            setTimeout(done, 1);
+        });
     });
-  });
 
-  describe(path.resolve(__dirname, '../database/GeoIPASNum.dat'), function() {
-    it('is asnum db', function(done) {
-      var file = path.resolve(__dirname, '../database/GeoIPASNum.dat');
-      var type = geoip.check(file);
+    describe(path.resolve(__dirname, '../database/GeoIPASNum.dat'), function() {
+        it('is asnum db', function(done) {
+            var file = path.resolve(__dirname, '../database/GeoIPASNum.dat');
+            var type = geoip.check(file);
 
-      type.should.equal('asnum');
-      setTimeout(done, 1);
+            type.should.equal('asnum');
+            setTimeout(done, 1);
+        });
     });
-  });
 
-  describe(path.resolve(__dirname, '../database/GeoIPNetSpeedCell.dat'), function() {
-    it('is netspeed cellular db', function(done) {
-      var file = path.resolve(__dirname, '../database/GeoIPNetSpeedCell.dat');
-      var type = geoip.check(file);
+    describe(path.resolve(__dirname, '../database/GeoIPNetSpeedCell.dat'), function() {
+        it('is netspeed cellular db', function(done) {
+            var file = path.resolve(__dirname, '../database/GeoIPNetSpeedCell.dat');
+            var type = geoip.check(file);
 
-      type.should.equal('netspeed cellular');
-      setTimeout(done, 1);
+            type.should.equal('netspeed cellular');
+            setTimeout(done, 1);
+        });
     });
-  });
 });
