091bdf2abd8d4209a86e0b29b7c22b832ebfbe5a
Guan Qing <kuno.the.developer@gmail.com>
2014-02-18 14:23:37 +0800

    Do not return/throw error when option silent set to true

--- test/city.mocha.js
+++ test/city.mocha.js
@@ -17,86 +17,107 @@ var should = chai.should();
 var geoip = require('../index.js');
 var City = geoip.City;
 var file = path.resolve(__dirname, '../database/GeoLiteCity.dat');
-var instance = new City(file, true);
 
 describe('City', function() {
-  describe('Instance', function() {
-    it('should be a object', function(done) {
-      instance.should.be.an('object');
-      setTimeout(done, 1);
-    });
+    describe('Instance', function() {
+        var instance = new City(file);
 
-    it('should has a lookup method', function(done) {
-      instance.lookup.should.be.a('function');
-      setTimeout(done, 1);
-    });
-
-    it('should has a lookupSync method', function(done) {
-      instance.lookupSync.should.be.a('function');
-      setTimeout(done, 1);
-    });
-
-    it('should has a update method', function(done) {
-      instance.update.should.be.a('function');
-      setTimeout(done, 1);
-    });
-  });
-
-  describe('Synchrouns Lookup', function() {
-    it('should throw error when input is not a string', function(done) {
-        try {
-            instance.lookupSync(null);
-        } catch(err) {
-            should.exist(err);
+        it('should be a object', function(done) {
+            instance.should.be.an('object');
             setTimeout(done, 1);
-        }
-    });
+        });
 
-    it('should can find location by domain', function(done) {
-      var data = instance.lookupSync('www.google.com');
-      data.should.be.a('object');
-      setTimeout(done, 1);
-    });
-
-    it('should can find location by ip address', function(done) {
-      var data = instance.lookupSync('8.8.8.8');
-      data.should.be.a('object');
-      setTimeout(done, 1);
-    });
-  });
-
-  describe('Asynchrouns Lookup', function() {
-    it('should return error when input is not a string', function(done) {
-        instance.lookup(null, function(err, data) {
-            should.exist(err);
+        it('should has a lookup method', function(done) {
+            instance.lookup.should.be.a('function');
             setTimeout(done, 1);
+        });
+
+        it('should has a lookupSync method', function(done) {
+            instance.lookupSync.should.be.a('function');
+            setTimeout(done, 1);
+        });
+
+        it('should has a update method', function(done) {
+            instance.update.should.be.a('function');
+            setTimeout(done, 1);
+        });
+
+        describe('Synchrouns Lookup', function() {
+            it('should throw error when input is not a string', function(done) {
+                try {
+                    instance.lookupSync(null);
+                } catch(err) {
+                    should.exist(err);
+                    setTimeout(done, 1);
+                }
+            });
+
+            it('should can find location by domain', function(done) {
+                var data = instance.lookupSync('www.google.com');
+                data.should.be.a('object');
+                setTimeout(done, 1);
+            });
+
+            it('should can find location by ip address', function(done) {
+                var data = instance.lookupSync('8.8.8.8');
+                data.should.be.a('object');
+                setTimeout(done, 1);
+            });
+        });
+
+        describe('Asynchrouns Lookup', function() {
+            it('should return error when input is not a string', function(done) {
+                instance.lookup(null, function(err, data) {
+                    should.exist(err);
+                    setTimeout(done, 1);
+                });
+            });
+
+            it('should can find location by domain', function(done) {
+                instance.lookup('www.google.com', function(err, data) {
+                    should.not.exist(err);
+                    should.exist(data);
+                    data.should.be.an('object');
+                    setTimeout(done, 1);
+                });
+            });
+
+            it('should can find location by ip address', function(done) {
+                instance.lookup('8.8.8.8', function(err, data) {
+                    should.not.exist(err);
+                    should.exist(data);
+                    data.should.be.an('object');
+                    setTimeout(done, 1);
+                });
+            });
+        });
+
+        describe('Update database on the fly', function() {
+            it('should be ok', function(done) {
+                //expect(instance.update('/tmp')).to.throw(Error);
+                instance.update(file).should.be.ok;
+                setTimeout(done, 1);
+            });
         });
     });
 
-    it('should can find location by domain', function(done) {
-      instance.lookup('www.google.com', function(err, data) {
-        should.not.exist(err);
-        should.exist(data);
-        data.should.be.an('object');
-        setTimeout(done, 1);
-      });
-    });
+    describe('Silent instance', function() {
+        var instance = new City(file, {silent: true});
+        describe('Synchrouns Lookup', function() {
+            it('should return null when input is not a string', function(done) {
+                should.equal(instance.lookupSync(null), null);
+                setTimeout(done, 1);
+            });
+        });
 
-    it('should can find location by ip address', function(done) {
-      instance.lookup('8.8.8.8', function(err, data) {
-        should.not.exist(err);
-        should.exist(data);
-        data.should.be.an('object');
-        setTimeout(done, 1);
-      });
+        describe('Asynchrouns Lookup', function() {
+            it('should return null when input is not a string', function(done) {
+                instance.lookup(null, function(err, data) {
+                    should.not.exist(err);
+                    should.equal(data, null);
+                    setTimeout(done, 1);
+                });
+            });
+        });
     });
-  });
-
-  describe('Update database on the fly', function() {
-    it('should be ok', function(done) {
-      //expect(instance.update('/tmp')).to.throw(Error);
-      instance.update(file).should.be.ok;
-      setTimeout(done, 1);
-    });
-  });
 });
