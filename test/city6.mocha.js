0ed4923b52d104909f1881ee3e0637b95a67a0f4
Guan Qing <kuno.the.developer@gmail.com>
2014-03-02 16:36:38 +0800

    Add {slient:truee} test to all classes

--- test/city6.mocha.js
+++ test/city6.mocha.js
@@ -6,7 +6,7 @@ if (semver.gte(process.version, '0.6.0') && semver.lt(process.version, '0.11.0')
         warning('Memory leak detected: %j', info);
     });
 }
-   
+
 var path = require('path');
 var mocha = require('mocha');
 var chai = require('chai');
@@ -17,85 +17,106 @@ var should = chai.should();
 var geoip = require('../index.js');
 var City6 = geoip.City6;
 var file = path.resolve(__dirname, '../database/GeoLiteCityv6.dat');
-var instance = new City6(file, true);
 
 describe('City6', function() {
-  describe('Instance', function() {
-    it('should be a object', function(done) {
-      instance.should.be.an('object');
-      setTimeout(done, 1);
-    });
+    describe('Instance', function() {
+        var instance = new City6(file, true);
 
-    it('should has a lookup method', function(done) {
-      instance.lookup.should.be.a('function');
-      setTimeout(done, 1);
-    });
-
-    it('should has a lookupSync method', function(done) {
-      instance.lookupSync.should.be.a('function');
-      setTimeout(done, 1);
-    });
-
-    it('should has a update method', function(done) {
-      instance.update.should.be.a('function');
-      setTimeout(done, 1);
-    });
-  });
-
-  describe('Synchrouns Lookup', function() {
-    it('should throw error when input is not a string', function(done) {
-        try {
-            instance.lookupSync(null);
-        } catch(err) {
-            should.exist(err);
+        it('should be a object', function(done) {
+            instance.should.be.an('object');
             setTimeout(done, 1);
-        }
-    });
+        });
 
-    it('should can find location by domain', function(done) {
-      var data = instance.lookupSync('www.google.com');
-      data.should.be.a('object');
-      setTimeout(done, 1);
-    });
-
-    it('should can find location by ip address', function(done) {
-      var data = instance.lookupSync('2406:a000:f0ff:fffe::122d');
-      data.should.be.a('object');
-      setTimeout(done, 1);
-    });
-  });
-
-  describe('Asynchrouns Lookup', function() {
-    it('should return error when input is not a string', function(done) {
-        instance.lookup(null, function(err, data) {
-            should.exist(err);
+        it('should has a lookup method', function(done) {
+            instance.lookup.should.be.a('function');
             setTimeout(done, 1);
+        });
+
+        it('should has a lookupSync method', function(done) {
+            instance.lookupSync.should.be.a('function');
+            setTimeout(done, 1);
+        });
+
+        it('should has a update method', function(done) {
+            instance.update.should.be.a('function');
+            setTimeout(done, 1);
+        });
+
+        describe('Synchrouns Lookup', function() {
+            it('should throw error when input is not a string', function(done) {
+                try {
+                    instance.lookupSync(null);
+                } catch(err) {
+                    should.exist(err);
+                    setTimeout(done, 1);
+                }
+            });
+
+            it('should can find location by domain', function(done) {
+                var data = instance.lookupSync('www.google.com');
+                data.should.be.a('object');
+                setTimeout(done, 1);
+            });
+
+            it('should can find location by ip address', function(done) {
+                var data = instance.lookupSync('2406:a000:f0ff:fffe::122d');
+                data.should.be.a('object');
+                setTimeout(done, 1);
+            });
+        });
+
+        describe('Asynchrouns Lookup', function() {
+            it('should return error when input is not a string', function(done) {
+                instance.lookup(null, function(err, data) {
+                    should.exist(err);
+                    setTimeout(done, 1);
+                });
+            });
+
+            it('should can find location by domain', function(done) {
+                instance.lookup('www.google.com', function(err, data) {
+                    should.not.exist(err);
+                    should.exist(data);
+                    data.should.be.an('object');
+                    setTimeout(done, 1);
+                });
+            });
+
+            it('should can find location by ip address', function(done) {
+                instance.lookup('2607:f0d0:1002:0051:0000:0000:0000:0004', function(err, data) {
+                    should.not.exist(err);
+                    should.exist(data);
+                    data.should.be.an('object');
+                    setTimeout(done, 1);
+                });
+            });
+        });
+
+        describe('Update database on the fly', function() {
+            it('should be ok', function(done) {
+                instance.update(file).should.be.ok;
+                setTimeout(done, 1);
+            });
         });
     });
 
-    it('should can find location by domain', function(done) {
-      instance.lookup('www.google.com', function(err, data) {
-        should.not.exist(err);
-        should.exist(data);
-        data.should.be.an('object');
-        setTimeout(done, 1);
-      });
-    });
+    describe('Silent instance', function() {
+        var instance = new City6(file, {silent: true});
+        describe('Synchrouns Lookup', function() {
+            it('should return null when input is not a string', function(done) {
+                should.equal(instance.lookupSync(null), null);
+                setTimeout(done, 1);
+            });
+        });
 
-    it('should can find location by ip address', function(done) {
-      instance.lookup('2607:f0d0:1002:0051:0000:0000:0000:0004', function(err, data) {
-        should.not.exist(err);
-        should.exist(data);
-        data.should.be.an('object');
-        setTimeout(done, 1);
-      });
+        describe('Asynchrouns Lookup', function() {
+            it('should return null when input is not a string', function(done) {
+                instance.lookup(null, function(err, data) {
+                    should.not.exist(err);
+                    should.equal(data, null);
+                    setTimeout(done, 1);
+                });
+            });
+        });
     });
-  });
-
-  describe('Update database on the fly', function() {
-    it('should be ok', function(done) {
-      instance.update(file).should.be.ok;
-      setTimeout(done, 1);
-    });
-  });
 });
--- test/netspeedcell.mocha.js
+++ test/netspeedcell.mocha.js
@@ -16,87 +16,108 @@ var should = chai.should();
 
 var NetSpeedCell = require('../lib/netspeedcell');
 var file = path.resolve(__dirname, '../database/GeoIPNetSpeedCell.dat');
-var instance = new NetSpeedCell(file, true);
 
 describe('NetSpeedCell', function() {
-  describe('Instance', function() {
-    it('should be a object', function(done) {
-      instance.should.be.an('object');
-      setTimeout(done, 1);
-    });
+    describe('Instance', function() {
+        var instance = new NetSpeedCell(file, true);
 
-    it('should has a lookup method', function(done) {
-      instance.lookup.should.be.a('function');
-      setTimeout(done, 1);
-    });
-
-    it('should has a lookupSync method', function(done) {
-      instance.lookupSync.should.be.a('function');
-      setTimeout(done, 1);
-    });
-
-    it('should has a update method', function(done) {
-      instance.update.should.be.a('function');
-      setTimeout(done, 1);
-    });
-  });
-
-  describe('Synchrouns Lookup', function() {
-    it('should throw error when input is not a string', function(done) {
-        try {
-            instance.lookupSync(null);
-        } catch(err) {
-            should.exist(err);
+        it('should be a object', function(done) {
+            instance.should.be.an('object');
             setTimeout(done, 1);
-        }
-    });
+        });
 
-    it.skip('should can find speed by domain', function(done) {
-      var data = instance.lookupSync('www.163.com');
-      data.should.be.a('string');
-      setTimeout(done, 1);
-    });
-
-    it('should can find speed by ip address', function(done) {
-      var data = instance.lookupSync('151.42.234.143');
-      data.should.be.a('string');
-      data.should.equal("Cable/DSL");
-      setTimeout(done, 1);
-    });
-  });
-
-  describe('Asynchrouns Lookup', function() {
-   it('should return error when input is not a string', function(done) {
-        instance.lookup(null, function(err, data) {
-            should.exist(err);
+        it('should has a lookup method', function(done) {
+            instance.lookup.should.be.a('function');
             setTimeout(done, 1);
+        });
+
+        it('should has a lookupSync method', function(done) {
+            instance.lookupSync.should.be.a('function');
+            setTimeout(done, 1);
+        });
+
+        it('should has a update method', function(done) {
+            instance.update.should.be.a('function');
+            setTimeout(done, 1);
+        });
+
+        describe('Synchrouns Lookup', function() {
+            it('should throw error when input is not a string', function(done) {
+                try {
+                    instance.lookupSync(null);
+                } catch(err) {
+                    should.exist(err);
+                    setTimeout(done, 1);
+                }
+            });
+
+            it.skip('should can find speed by domain', function(done) {
+                var data = instance.lookupSync('www.163.com');
+                data.should.be.a('string');
+                setTimeout(done, 1);
+            });
+
+            it('should can find speed by ip address', function(done) {
+                var data = instance.lookupSync('151.42.234.143');
+                data.should.be.a('string');
+                data.should.equal("Cable/DSL");
+                setTimeout(done, 1);
+            });
+        });
+
+        describe('Asynchrouns Lookup', function() {
+            it('should return error when input is not a string', function(done) {
+                instance.lookup(null, function(err, data) {
+                    should.exist(err);
+                    setTimeout(done, 1);
+                });
+            });
+
+            it.skip('should can find speed by domain', function(done) {
+                instance.lookup('www.163.com', function(err, data) {
+                    should.not.exist(err);
+                    should.exist(data);
+                    data.should.be.an('string');
+                    setTimeout(done, 1);
+                });
+            });
+
+            it('should can find speed by ip address', function(done) {
+                instance.lookup('212.245.206.97', function(err, data) {
+                    should.not.exist(err);
+                    should.exist(data);
+                    data.should.be.an('string');
+                    data.should.equal("Dialup");
+                    setTimeout(done, 1);
+                });
+            });
+        });
+
+        describe('Update database on the fly', function() {
+            it('should be ok', function(done) {
+                instance.update(file).should.be.ok;
+                setTimeout(done, 1);
+            });
         });
     });
 
-    it.skip('should can find speed by domain', function(done) {
-      instance.lookup('www.163.com', function(err, data) {
-        should.not.exist(err);
-        should.exist(data);
-        data.should.be.an('string');
-        setTimeout(done, 1);
-      });
-    });
+    describe('Silent instance', function() {
+        var instance = new NetSpeedCell(file, {silent: true});
+        describe('Synchrouns Lookup', function() {
+            it('should return null when input is not a string', function(done) {
+                should.equal(instance.lookupSync(null), null);
+                setTimeout(done, 1);
+            });
+        });
 
-    it('should can find speed by ip address', function(done) {
-      instance.lookup('212.245.206.97', function(err, data) {
-        should.not.exist(err);
-        should.exist(data);
-        data.should.be.an('string');
-        data.should.equal("Dialup");
-        setTimeout(done, 1);
-      });
+        describe('Asynchrouns Lookup', function() {
+            it('should return null when input is not a string', function(done) {
+                instance.lookup(null, function(err, data) {
+                    should.not.exist(err);
+                    should.equal(data, null);
+                    setTimeout(done, 1);
+                });
+            });
+        });
     });
-  });
-
-  describe('Update database on the fly', function() {
-    it('should be ok', function(done) {
-      instance.update(file).should.be.ok;
-      setTimeout(done, 1);
-    });
-  });
 });
