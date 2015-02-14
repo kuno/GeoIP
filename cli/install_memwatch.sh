c2ac17a93098d95dcdc4ecad8a0396413707b0b4
kuno <kuno.the.developer@gmail.com>
2015-02-14 19:10:44 +0800

    Make sure trivis tests working on nodejs 0.8/0.10

--- cli/install_memwatch.sh
+++ cli/install_memwatch.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-NODE_VERSION=`node --version | grep -i v0.12.*`
-IOJS_VERSION='iojs --version'
+NODE_VERSION=`node --version | grep -i v0.1[1-9].*`
+IOJS_VERSION=`iojs --version`
 
-echo $NODE_VERSION
+echo '#######'
+echo "Node Version ${NODE_VERSION}"
+echo "Iojs Version ${IOJS_VERSION}"
+echo '#######'
 
-if [[ ! $NODE_VERSION && ! IOJS_VERSION ]]; then
+if [[ ! $NODE_VERSION && ! $IOJS_VERSION ]]; then
+    echo "Going to install memwatch"
     npm install memwatch
 else
+    echo "Going to uninstall memwatch"
     npm remove memwatch
 fi
